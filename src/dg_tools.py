aa8ed8dde3b0070cfb36ea7b1770f84833b4faed
fabianfa <fabian.faulstich@yahoo.de>
2020-07-23 20:16:25 +0200

    added get_V_net to dg_tools

--- src/dg_tools.py
+++ src/dg_tools.py
@@ -1,6 +1,5 @@
 import numpy as np
-from scipy.spatial import Delaunay
-
+from scipy.spatial import Delaunay, Voronoi
 
 def unfold(m,n):
     l = m % n
@@ -34,3 +33,104 @@ def in_hull(p, hull):
     return hull.find_simplex(p)>=0
 
 
+def get_V_net(atoms, x_min, x_max, y_min, y_max):
+    """
+    Only 2D for now
+    atoms:
+    box parameters:
+    """
+
+    # Get "innter" vertices from scipy
+    vor  = Voronoi(atoms)
+    edge = vor.ridge_vertices
+    vert = vor.vertices
+
+    # Compute vertices on the boundary
+    center = atoms.mean(axis=0)
+    for j, (pointidx, rv) in enumerate(zip(vor.ridge_points, edge)):
+        rv = np.asarray(rv)
+        if np.any(rv < 0):
+            i = rv[rv >= 0][0]
+            t = atoms[pointidx[1]]- atoms[pointidx[0]]
+            t = t/np.linalg.norm(t)
+            n = np.array([-t[1],t[0]])
+            midpoint = atoms[pointidx].mean(axis=0)
+            normal = np.sign(np.dot(midpoint-center, n))*n
+            print(normal)
+            if normal[0] < 0 and normal[1] < 0:
+                dx = vert[i][0]-x_min
+                dy = vert[i][1]-y_min
+                scalar = min(np.sign(normal[0])*dx/normal[0] ,np.sign(normal[1])*dy/normal[1] )
+            elif normal[0] > 0 and normal[1] > 0:
+                dx = x_max-vert[i][0]
+                dy = y_max-vert[i][1]
+                scalar = min(np.sign(normal[0])*dx/normal[0] ,np.sign(normal[1])*dy/normal[1] )
+            elif normal[0] < 0 and normal[1] > 0:
+                dx = vert[i][0]-x_min
+                dy = y_max-vert[i][1]
+                scalar = min(np.sign(normal[0])*dx/normal[0] ,np.sign(normal[1])*dy/normal[1] )
+            elif normal[0] > 0 and normal[1] < 0:
+                dx = x_max-vert[i][0]
+                dy = vert[i][1]-y_min
+                scalar = min(np.sign(normal[0])*dx/normal[0] ,np.sign(normal[1])*dy/normal[1] )
+            elif normal[0] == 0 and normal[1] < 0:
+                dy = vert[i][1]-y_min
+                scalar = np.sign(normal[1])*dy/normal[1] 
+            elif normal[0] == 0 and normal[1] > 0:
+                dy = y_max-vert[i][1]
+                scalar = np.sign(normal[1])*dy/normal[1] 
+            elif normal[0] < 0 and normal[1] == 0:
+                dx = vert[i][0]-x_min
+                scalar = np.sign(normal[0])*dx/normal[0] 
+            elif normal[0] > 0 and normal[1] == 0:
+                dx = x_max-vert[i][0]
+                scalar = np.sign(normal[0])*dx/normal[0]
+
+            boundary_point = vert[i] + np.sign(np.dot(midpoint-center, n))*n*scalar
+            vert = np.vstack((vert, boundary_point))
+            edge[j][np.argmin(rv)] = len(vert) #only 2D for now
+
+    # Add boundary vertices and edges
+    vert = np.vstack((vert, np.array([x_min, y_min])))
+    vert = np.vstack((vert, np.array([x_max, y_min])))
+    vert_s = np.array([v for v in vert if v[1] == y_min])
+    vert_s = np.array(sorted(vert_s, key=lambda x: x[0]))
+    for i, v in enumerate(vert_s[1:]):
+        idx_0 = np.where((vert[:,0] == vert_s[i][0]) & (vert[:,1]== vert_s[i][1]))[0][0]
+        idx_1 = np.where((vert[:,0] == v[0]) & (vert[:,1]==v[1]))[0][0]
+        edge.append([idx_0, idx_1])
+
+    vert = np.vstack((vert, np.array([x_max, y_max])))
+    vert_s = np.array([v for v in vert if v[0] == x_max])
+    vert_s = np.array(sorted(vert_s, key=lambda x: x[1]))
+    for i, v in enumerate(vert_s[1:]):
+        idx_0 = np.where((vert[:,0] == vert_s[i][0]) & (vert[:,1]== vert_s[i][1]))[0][0]
+        idx_1 = np.where((vert[:,0] == v[0]) & (vert[:,1]==v[1]))[0][0]
+        edge.append([idx_0, idx_1])
+
+    vert = np.vstack((vert, np.array([x_min, y_max])))
+    vert_s = np.array([v for v in vert if v[1] == y_max])
+    vert_s = np.array(sorted(vert_s, key=lambda x: x[0]))
+    for i, v in enumerate(vert_s[1:]):
+        idx_0 = np.where((vert[:,0] == vert_s[i][0]) & (vert[:,1]== vert_s[i][1]))[0][0]
+        idx_1 = np.where((vert[:,0] == v[0]) & (vert[:,1]==v[1]))[0][0]
+        edge.append([idx_0, idx_1])
+
+    vert_s = np.array([v for v in vert if v[0] == x_min])
+    vert_s = np.array(sorted(vert_s, key=lambda x: x[1]))
+    for i, v in enumerate(vert_s[1:]):
+        idx_0 = np.where((vert[:,0] == vert_s[i][0]) & (vert[:,1]== vert_s[i][1]))[0][0]
+        idx_1 = np.where((vert[:,0] == v[0]) & (vert[:,1]==v[1]))[0][0]
+        edge.append([idx_0, idx_1])
+
+    # create Voronoi network
+
+    edge_inv = [[e[1],e[0]] for e in edge]
+    edge = edge + [[e[1],e[0]] for e in edge]
+    edge = np.array(sorted(edge, key=lambda x: x[0]))
+    V_net = []
+    for j, v in enumerate(vert):
+        idx = np.where((edge[:,0] == j))[0]
+        V_net.append([v, edge[idx][:,1]])
+
+    return V_net

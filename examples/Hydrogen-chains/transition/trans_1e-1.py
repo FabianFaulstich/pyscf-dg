Fixed normalization

examples/Hydrogen-chains/transition/trans_1e-1.py
@@ -39,15 +39,15 @@ def mol_size(Mol):
 
 if __name__ == '__main__':
 
-    f   = open("out_tol1e-1_n.txt", "w")
+    f   = open("out_tol1e-1.txt", "w")
     bs  = 5
 
     dgrid = [2,2,2] # 
     #bond  = np.array([1.7])
     #bond1 = np.array([3.6])
     #atoms = np.flip(np.linspace(2, 4, 2, dtype = int))
-    atoms = np.flip(np.linspace(2,30,15, dtype = int))
-    #atoms = np.array([16])
+    #atoms = np.flip(np.linspace(2,30,15, dtype = int))
+    atoms = np.array([8])
     #atoms = np.linspace(2, 8, num = 4, dtype = int)
     #print(atoms)
     #exit()
@@ -110,7 +110,12 @@ if __name__ == '__main__':
             cell.mesh    = np.array(mesh)
             cell.atom    = Mol
             cell.build()
-            
+           
+            mesh = cell.mesh
+            vol = cell.vol
+            ngrids = np.prod(mesh)
+            dvol = vol / ngrids
+
             nnz_eri_pw[i] = np.prod(mesh)**2
             
             # Creating mol object to fetch ao_projections
@@ -123,10 +128,16 @@ if __name__ == '__main__':
 
             coords = cell.get_uniform_grids()
             ao_values = mol.eval_gto("GTOval_sph", coords)
-            U0, _, _ = la.svd(ao_values, full_matrices = False)
-            print(ao_values.shape)
-            print(U0.shape)
-
+            U, _, VT = la.svd(ao_values, full_matrices = False)
+            
+            # U0 describes L^2 normalized functions.
+            # This is only necesary for using dg_tools.get_dg_nnz_eri(), because 
+            # dg_model_ham.gram is stored in that way. This will be changed in 
+            # future adaptation of the code. Direct input of the projections 
+            # matrix should always only consist of the nodel values of the 
+            # primitive basis. Note that the gram input for a dg_model_ham object             # takes the nodel values of the primitive basis as input for gram. 
+                       
+            U0 = np.dot(U, VT) / np.sqrt(dvol)
             del boxsize
             
             print("Computing Gram Matrix ...")